51d867db7e083c6b8a9c40e3d08851683d70b2a5
Anuradha987 <anualgama@gmail.com>
2021-04-01 22:51:42 +0530

    my project

---
+++ ProductsBLL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billing2
+{
+    class ProductsBLL
+    {
+        public int P_Id { get; set; }
+        public string P_Barcode { get; set; }
+        public string P_Name { get; set; }
+        public decimal P_Price { get; set; }
+        public byte[] P_Image { get; set; }
+    }
+}
---
+++ transactionDAL.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Billing2
+{
+    class transactionDAL
+    {
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+        public bool Insert_Transaction(transactionsBLL t, out int transactionID)
+        {
+            bool isSuccess = false;
+
+            transactionID = -1;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                string sql = "INSERT INTO tbl_transactions(grandTotal, transaction_date, tax, discount) VALUES (@grandTotal, @transaction_date, @tax, @discount); SELECT @@IDENTITY";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@grandTotal", t.grandTotal);
+                cmd.Parameters.AddWithValue("@transaction_date", t.transaction_date);
+                cmd.Parameters.AddWithValue("@tax", t.tax);
+                cmd.Parameters.AddWithValue("@discount", t.discount);
+
+                conn.Open();
+                object o = cmd.ExecuteScalar();
+                if (o != null)
+                {
+                    transactionID = int.Parse(o.ToString());
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false; 
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+    }
+}
